fe38b4ac338742cbb3b4417eed2c51794ab609ab
fsmosca <fsmosca@users.noreply.github.com>
2020-06-08 04:45:45 +0800

    Add sample command line with static eval flag

--- qpe.bat
+++ qpe.bat
@@ -1,7 +1,10 @@
-set MTMS=200
+set MTMS=500
 set EPD="./epd/wacnew.epd"
 
-python qpe.py --input %EPD% --movetimems %MTMS% --engine "./engine/stockfish-11-win/stockfish_20011801_x64_modern.exe" --engineoption "Hash=128"
+:: python qpe.py --input %EPD% --movetimems %MTMS% --engine "./engine/stockfish-11-win/stockfish_20011801_x64_modern.exe" --engineoption "Hash=128"
+
+:: Use static eval
+python qpe.py --input %EPD% --movetimems %MTMS% --engine "./engine/stockfish-11-win/stockfish_20011801_x64_modern.exe" --engineoption "Hash=128" --static-eval
 
 
 pause
