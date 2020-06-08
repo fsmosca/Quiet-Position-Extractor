5c1550095e1f92352ebab2ebc8d60b3aeedb1bca
fsmosca <fsmosca@users.noreply.github.com>
2020-06-08 12:02:58 +0800

    Update qpe.bat

--- qpe.bat
+++ qpe.bat
@@ -1,10 +1,10 @@
-set MTMS=500
+set MTMS=1500
 set EPD="./epd/wacnew.epd"
 
-:: python qpe.py --input %EPD% --movetimems %MTMS% --engine "./engine/stockfish-11-win/stockfish_20011801_x64_modern.exe" --engineoption "Hash=128"
+:: python qpe.py --input %EPD% --movetimems %MTMS% --engine "./engine/stockfish-11-win/stockfish_20011801_x64_modern.exe" --engine-option "Hash=128"
 
 :: Use static eval
-python qpe.py --input %EPD% --movetimems %MTMS% --engine "./engine/stockfish-11-win/stockfish_20011801_x64_modern.exe" --engineoption "Hash=128" --static-eval
+python qpe.py --input %EPD% --movetimems %MTMS% --engine "./engine/stockfish-11-win/stockfish_20011801_x64_modern.exe" --engine-option "Hash=128" --static-eval
 
 
 pause
